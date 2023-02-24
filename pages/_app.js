61326ba68155e9dc010842c0d072efe5b2a337bb
chakrahun <108547535+chakrahun@users.noreply.github.com>
2023-02-24 13:50:50 +1100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
-import '@styles/globals.css'
-
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default Application
+<a class="weatherwidget-io" href="https://forecast7.com/en/n37d81144d96/melbourne/" data-label_1="Melbourne" data-label_2="Mood" data-font="Georgia" data-theme="dark" data-shadow="#64473A" data-accent="rgba(0, 0, 0, 0.12)" data-textcolor="#64473A" data-highcolor="#64473A" data-lowcolor="#64473A" data-suncolor="#64473A" data-mooncolor="#64473A" data-cloudcolor="#64473A" data-cloudfill="#64473A" data-raincolor="#64473A" data-snowcolor="#64473A" >Melbourne Mood</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
